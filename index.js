9db7e847aad5f84dbd78293ce550cd83213e788e
Ian Williams <ian.williams@primaltalk.com>
2016-06-09 17:55:52 +0100

    Added the "p4 files" command.

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ NodeP4.prototype.users = function (options, callback) {
 var commonCommands = ['add', 'delete', 'edit', 'revert', 'sync',
                       'diff', 'reconcile', 'reopen', 'resolved',
                       'shelve', 'unshelve', 'client', 'resolve',
-                      'submit', 'set', 'depots', 'depot'];
+                      'submit', 'set', 'depots', 'depot', 'files'];
 commonCommands.forEach(function (command) {
   NodeP4.prototype[command] = function (options, callback) {
     execP4(command, options, callback);
