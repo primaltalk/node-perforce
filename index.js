31c311ed9c03c0658f6112bb1acc38ac252be48a
Ian Williams <ian.williams@primaltalk.com>
2016-06-15 15:02:11 +0100

    Added detection of non-error condition where files not opened in client.

--- index.js
+++ index.js
@@ -71,8 +71,9 @@ function execP4(p4cmd, options, callback) {
     if (stderr) {
       console.log('STDERR: "' + stderr + '"');
       // Need to trap where a sync is executed but the files are up to date, which
-      // is not in and of itself an error.
-      if(stderr.indexOf('File(s) up-to-date.') >= 0) {
+      // is not in and of itself an error.  Ditto for files not open on this client.
+      if(stderr.indexOf('File(s) up-to-date.') >= 0
+         || stderr.indexOf('File(s) not opened on this client.') >= 0) {
         return callback(null, stderr);
       }
       return callback(new Error(stderr));
